ece84c8871fc51648db0f897008ec0280f3b2309
挽枫 <wanfeng@wanfengdeMacBook-Pro.local>
2022-10-19 18:06:19 +0800

    feat:模板增加easyv-cli

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
-const chalk = require('chalk');
-const str = chalk.hex('#DEADED')('111111');
-console.info(str);
+const download = require('download-git-repo');
+download("direct:http://gitlab.prod.dtstack.cn/visdev/easyv-cli.git",'master',{clone:true},function (err) {
+    console.log(err ? 'Error' : 'Success');
+    console.log(err);
+})  
